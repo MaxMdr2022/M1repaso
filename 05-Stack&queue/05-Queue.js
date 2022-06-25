8ee59db36a2e8f79051358c686e241c50bbbac73
MaxMdr2022 <maxi_meder94@hotmail.com>
2022-06-25 19:08:28 -0300

    homework 05

--- 05-Stack&queue/05-Queue.js
+++ 05-Stack&queue/05-Queue.js
@@ -18,13 +18,53 @@ const { Queue } = require("../estructuras")
 // Importante!: Aquellas personas que no cumplan con los requisitos para ingresar deben ser removidos de la cola 
 
 var controlAcces = function(queue, event){
-    // Tu código aca:
-    
-  };
+  // Tu código aca:
+  let nombres = [];
+  
+  for(var i=0; i<queue.array.length; i++){
+    for(var j=i+1; j<queue.array.length; j++){
+
+      if(queue.array[i]["ticket"].number === queue.array[j]["ticket"].number && queue.array[i]["ticket"]["event"] === queue.array[j]["ticket"]["event"]){
+
+        queue.array.splice(j,1);
+        
+      }
+    }
+
+  }
+
+  for (var i=0; i < queue.array.length; i++){
+      // tengo que recorrer el array que tiene todos los objetos y preguntar si esa persona(obeto) cumple con los requisitos 
+      // cada i va a ser una persona, entonces esas preguntas las hago aca dentro del for.
+      // en caso de no cumplir con alguna condicion, como la saco? con splice.
+      // queue.array.splice(i,1);
+      //tengo que pushear en un arreglo los nombres y el numero de ticket, si ese numero de ticket coincide con otro 
+      // saco a ese elemento del queue.array ( a esa persona)
+    if(queue.array[i]["age"] < 18){
+
+      queue.array.splice(i,1);
+      i=i-1
+
+    }else if(queue.array[i]["ticket"]["event"] !== event ){
+
+      queue.array.splice(i,1);
+      i=i-1
+    }else{
+
+      nombres.push(queue.array[i]["fullname"]);
+    }
+  }
+  // console.log("----------------------------------------------------dsdsdsd-----------")
+  // console.log(queue.array[0]["ticket"].number);
+  // console.log(queue.array.length)
+  return nombres;
+
+};
       
+
   
 
-  module.exports = {
-    controlAcces,
+module.exports = {
+  controlAcces,
    
 }
--- 05-Stack&queue/05-stack.js
+++ 05-Stack&queue/05-stack.js
@@ -3,7 +3,7 @@ const { Stack, Queue } = require("../estructuras")
 // Utilizando un STACK, y dado un string, invertir el orden de las palabras.
 // OJO: Para este ejercicio NO se pueden usar metodos de Array.
 // Deben utilizar solo los metodos provistos por la STACK
-
+ 
 // Parametro:
 //  (String): string con un texto cargado de palabras
  
@@ -17,7 +17,34 @@ const { Stack, Queue } = require("../estructuras")
 
 function efectoEspejo(str){  
 //tu codigo aqui
+var elemento= "";
 
+for(var i=0; i<str.length; i++){
+
+    if(str[i] === " " || i == str.length-1){
+
+        for(var j=i; j>= 0; j--){
+
+            if(str[i] === " "){
+                i = i -1;
+                j= i 
+            }
+            if( elemento[j] !== " "){
+
+                elemento = elemento + str[j]
+            }
+            if(str[j] === " " && i !== str.length-1 || j === 0){
+                elemento = elemento + " ";
+            }
+            if( str[j] === " " || j=== 0 ){
+                j = 0
+                i = i +1;
+            }
+
+        }
+    }
+}
+return elemento;
 };
 
 
